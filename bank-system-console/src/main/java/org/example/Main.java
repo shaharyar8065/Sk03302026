41e3cfde9008aa7af1e4817f7b438a1a629f4f0f
shaharyar8065 <shaharyar8065@gmail.com>
2026-05-07 13:20:05 -0400

    created BankAccount

--- bank-system-console/src/main/java/org/example/Main.java
+++ bank-system-console/src/main/java/org/example/Main.java
@@ -1,36 +1,69 @@
 package org.example;
 
+import org.example.dtos.TransferRequest;
 import org.example.entities.BankAccount;
-import org.example.entities.CheckingsAccount;
-import org.example.entities.SavingsAccount;
-import org.example.entities.Transferable;
+import org.example.enums.Priority;
 
-import javax.sql.rowset.serial.SQLOutputImpl;
 import java.math.BigDecimal;
-import java.util.List;
 
-public class Main{
-    public static void main(String[]args){
-        List<BankAccount> listofAccounts = List.of(
-                new SavingsAccount("dsbh23-3hdx",new BigDecimal("450.00"), new BigDecimal("4.5")),
-                new SavingsAccount("vgdhs-dsvchx",new BigDecimal("1000.00"),new BigDecimal("6.1")),
-                new CheckingsAccount("dhch2s-514kx",new BigDecimal("1600.00"), new BigDecimal("10.00"))
-        );
+public class Main {
+    public static void main() {
+        BankAccount account = BankAccount
+                .newChecking("JSKADNASSAJFN(*#@(*H9832", new BigDecimal("5000"));
 
-//        System.out.println(listofAccounts.get(0).equals(listofAccounts.get(0)));
+        TransferRequest request = new TransferRequest
+                .Builder("JSKADNASSAJFN(*#@(*H9832",
+                "NUdsih79rh92fh92h",
+                new BigDecimal("250")).priority(Priority.HIGH).build();
+        TransferRequest request1 = new TransferRequest.Builder("JSKADNASSAJFN(*#@(*H9832",
+                "NUdsih79rh92fh92h",
+                new BigDecimal("250"))
+                .note("Payback")
+                .priority(Priority.LOW)
+                .build();
 
-        Transferable[]transferables = new Transferable[10];
-                int count = 0;
-        for (BankAccount account : listofAccounts){
-            account.applyMonthlyFee();
-            if(account instanceof Transferable transferable){
-                transferables[count++] = transferable;
-            }
-        }
+        if(request.getAmount().compareTo(BigDecimal.ZERO) > 0 && request.getAmount().compareTo(request1.getAmount()) <= 0) {
 
-        listofAccounts.forEach(System.out::println);
-        for (Transferable transferable : transferables){
-            System.out.println(transferable);
         }
     }
 }
+
+
+
+
+
+//
+//import org.example.entities.BankAccount;
+//import org.example.entities.CheckingsAccount;
+//import org.example.entities.SavingsAccount;
+//import org.example.entities.Transferable;
+//
+//import javax.sql.rowset.serial.SQLOutputImpl;
+//import java.math.BigDecimal;
+//import java.util.List;
+//
+//public class Main{
+//    public static void main(String[]args){
+//        List<BankAccount> listofAccounts = List.of(
+//                new SavingsAccount("dsbh23-3hdx",new BigDecimal("450.00"), new BigDecimal("4.5")),
+//                new SavingsAccount("vgdhs-dsvchx",new BigDecimal("1000.00"),new BigDecimal("6.1")),
+//                new CheckingsAccount("dhch2s-514kx",new BigDecimal("1600.00"), new BigDecimal("10.00"))
+//        );
+//
+////        System.out.println(listofAccounts.get(0).equals(listofAccounts.get(0)));
+//
+//        Transferable[]transferables = new Transferable[10];
+//                int count = 0;
+//        for (BankAccount account : listofAccounts){
+//            account.applyMonthlyFee();
+//            if(account instanceof Transferable transferable){
+//                transferables[count++] = transferable;
+//            }
+//        }
+//
+//        listofAccounts.forEach(System.out::println);
+//        for (Transferable transferable : transferables){
+//            System.out.println(transferable);
+//        }
+//    }
+//}
--- bank-system-console/src/main/java/org/example/entities/BankAccount.java
+++ bank-system-console/src/main/java/org/example/entities/BankAccount.java
@@ -2,54 +2,80 @@ package org.example.entities;
 
 import java.math.BigDecimal;
 
-    public abstract class BankAccount {
+    public class BankAccount {
     private long id;
     private String accountNumber;
     private BigDecimal balance;
+    private String type;
     private boolean active;
 
 
-    public BankAccount(){}
+    private BankAccount(){}
 
-    public BankAccount(String accountNumber, BigDecimal initialBalance) {
+    private BankAccount(String accountNumber, BigDecimal initialBalance) {
         if(initialBalance.compareTo(BigDecimal.ZERO) < 0 ) {
             throw new RuntimeException("Initial Balance has to be greater than or equal to zero");
         }
         this.id = 0;
         this.accountNumber = accountNumber;
+        this.type = "CHECKING";
         this.balance = initialBalance;
         this.active = true;
 
     }
 
-    public BankAccount(long id, String accountNumber, BigDecimal balance, boolean isActive) {
+    private BankAccount(long id, String accountNumber, BigDecimal balance, String type, boolean isActive) {
         if (balance.compareTo(BigDecimal.ZERO) < 0){
             throw new RuntimeException("Initial Balance has to be greater than or equal to zero");
         }
         this.id = id;
         this.accountNumber = accountNumber;
         this.balance = balance;
+        this.type = type;
         this.active = isActive;
     }
-    public abstract void applyMonthlyFee();
+
+    public static BankAccount newChecking(String num,BigDecimal amount){
+        return new BankAccount(num, amount);
+    }
+
+    public static BankAccount newSaving(String num, BigDecimal amount) {
+        return new BankAccount(0 ,num, amount, "SAVINGS", true);
+    }
+
+    public static BankAccount newBusiness(String num, BigDecimal amount) {
+        return new BankAccount(0, num, amount, "BUSINESS", true);
+    }
 
     public void deposit(BigDecimal amount) {
-       if(amount.compareTo(BigDecimal.ZERO) < 0) {
-           throw new RuntimeException("Amount passed is less than zero");
-       }
-       this.balance.add(amount);
+        this.balance.add(amount);
     }
 
-    public void withdraw(BigDecimal amount){
-        if(amount.compareTo(BigDecimal.ZERO) >=0 ){
-            throw new RuntimeException("Amount passed is less then zero");
-        }
-        if(balance.compareTo(amount) <=0 ) {
-            throw new RuntimeException("Balance must be greater than zero");
-        }
-        this.balance.subtract(amount);
-
+    public void withdraw(BigDecimal amount) {
+            balance.subtract(amount);
     }
+
+
+
+  //  public abstract void applyMonthlyFee();
+
+//    public void deposit(BigDecimal amount) {
+//       if(amount.compareTo(BigDecimal.ZERO) < 0) {
+//           throw new RuntimeException("Amount passed is less than zero");
+//       }
+//       this.balance.add(amount);
+//    }
+//
+//    public void withdraw(BigDecimal amount){
+//        if(amount.compareTo(BigDecimal.ZERO) >=0 ){
+//            throw new RuntimeException("Amount passed is less then zero");
+//        }
+//        if(balance.compareTo(amount) <=0 ) {
+//            throw new RuntimeException("Balance must be greater than zero");
+//        }
+//        this.balance.subtract(amount);
+//
+//    }
 
 
     public Long getId() {
